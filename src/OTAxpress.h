06e170573b91009fbcf3ff1799df597d0a6eb104
Guru of Microcontroller <118795462+Rafeul1997@users.noreply.github.com>
2026-05-12 23:09:22 +0530

    Add OTAxpress class for OTA updates

---
+++ src/OTAxpress.h
@@ -0,0 +1,27 @@
+#ifndef OTAXPRESS_H
+#define OTAXPRESS_H
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <ArduinoOTA.h>
+
+class OTAxpress {
+  public:
+    OTAxpress(const char* ssid, const char* password);
+
+    void setHostname(const char* hostname);
+    void setOTAPassword(const char* otaPassword);
+    void setStatusLED(int pin);
+
+    void begin();
+    void handle();
+
+  private:
+    const char* _ssid;
+    const char* _password;
+    const char* _hostname = "OTAxpress-ESP32";
+    const char* _otaPassword = NULL;
+    int _ledPin = -1;
+};
+
+#endif
